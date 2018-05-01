726cff0f4416243df0d4fb325d7e88edbde5fbee
rizwan96 <rb021025@gmail.com>
2018-05-02 03:20:23 +0500

    Rizwan Add controller

---
+++ school management system/school management system/Controllers/schoolController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace school_management_system.Controllers
+{
+    public class schoolController : Controller
+    {
+        // GET: school
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: school/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: school/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: school/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: school/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: school/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: school/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: school/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
